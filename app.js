a885cc956fa705c4485a2c333857def9102dd3ff
Viraj Kaushalye <virajkaush@gmail.com>
2018-07-07 03:01:35 +0530

    set bootstrap, fix views

--- app.js
+++ app.js
@@ -1,19 +1,32 @@
 let express = require('express');
 let path = require('path');
+let mongoose = require('mongoose');
+let config = require('./config/database')
+
+// Connect to db
+mongoose.connect(config.database);
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+    console.log('Database connected.');
+});
 
 // Init app
 app = express();
 
 // View engine setup
-app.set('view', path.join(__dirname ,'views'));
+app.set('views', path.join(__dirname ,'views'));
 app.set('view engine', 'ejs');
 
 // Set public folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 
 // Test
-app.get('/', (res, req) => {
-    res.send('working..');
+app.get('/', (req, res) => {
+    res.render('index',{
+        title: 'Home',
+        heading: "Hello there"
+    });
 });
 
 // Start the server
